0c41ed5cebac052bf61f513f52e9acb2e520dd9b
Mark Galloway <mgallowa@ualberta.ca>
2015-07-06 18:15:22 -0600

    Refactor search JS and add FadeIn to ajax

--- static/js/search.js
+++ static/js/search.js
@@ -1,82 +1,90 @@
-var base_url = '/classifieds/?';
-var url = base_url;
 
+(function() {
 
-/*
- *  Window scroll function to fire off ajax requests
- *  based off of window height
- */
-function scrollListener() {
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 200) {
+var adsList = {
+    container: $("#ad-list"),
+    loading: $('#loading-indicator').hide(),
+    base_url: '/classifieds/?',
+    url: "",
 
-        // unbind to prevent duplicate requests
-        $(window).unbind('scroll');
+    config: {
+        effect: 'fadeIn',
+        speed: 1300
+    },
 
-        // make ajax request
-        loadAjax();
+    init: function(config) {
+        $.extend(adsList.config, config);
+
+        // Register _loading spinner
+        $(document)
+            .ajaxStart(function() {
+                adsList.loading.show();
+            })
+            .ajaxStop(function() {
+                adsList.loading.hide();
+            });
+
+        // Clear current results
+        adsList.container.empty()
+
+        // Reset URL
+        adsList.url = adsList.base_url;
+
+        // TODO: get values from search form and append to URL
+
+        adsList._load();
+    },
+    /*
+    * load new classified ads and update url based off of results.
+    * Resets scroll listener if more results exist
+    */
+    _load: function() {
+        $.ajax({
+            url: adsList.url,
+            success: function(data) {
+
+                // Update url for next page
+                next_url = $(data).filter('#next-link').attr('href');
+
+                // Strip the next-link from the html
+                ads = $(data).not('#next-link');
+
+                // Apply effect
+                ads.hide()[adsList.config.effect](adsList.config.speed);
+
+                // append ads(html) to list of ads
+                ads.appendTo(adsList.container);
+
+                if(next_url) {
+                    adsList.url = next_url
+                    // Bind scroll listener is there are more results
+                    $(window).scroll(adsList._listen);
+                }
+            },
+            error : function(xhr, errmsg, err) {
+                console.log(xhr.status + ": " + xhr.responseText);
+            }
+
+        });
+    },
+    /*
+     *  Window scroll function to fire off ajax requests
+     *  based off of window height
+     */
+    _listen: function() {
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 200) {
+            // unbind to prevent duplicate requests
+            $(window).unbind('scroll');
+
+            // load new data
+            adsList._load();
+        }
     }
 }
 
-/*
- * Load new classified ads and update url based off of results.
- * Resets scroll listener if more results exist
- */
-function loadAjax() {
-    $.ajax({
-        url: url,
-        success: function(data) {
+adsList.init();
 
-            // Update url for next page
-            next_url = $(data).filter('#next-link').attr('href');
+// Register _loadData on search button click
+$("#search-button").on('click', adsList.init);
 
-            // Strip the next-link from the html
-            ads = $(data).not('#next-link')
-
-            // append ads(html) to list of ads
-            $("#ad-list").append(ads);
-
-            if(next_url) {
-                url = next_url
-                // Bind scroll listener is there are more results
-                $(window).scroll(scrollListener);
-            }
-
-        },
-        error : function(xhr, errmsg, err) {
-            console.log(xhr.status + ": " + xhr.responseText);
-        }
-
-    });
-}
-
-/*
- *  Function to load initial data on document ready or search
- */
-function loadData() {
-    // Clear current results
-    $("#ad-list").empty()
-
-    // Reset URL
-    url = base_url;
-
-    // TODO: get values from search form and append to URL
-
-    loadAjax();
-}
-
-// Register loadData on document ready
-$(loadData);
-
-// Register loadData on search button click
-$("#search-button").click(loadData);
-
-
-// Register loading spinner
-var $loading = $('#loading-indicator').hide();
-$(document)
-  .ajaxStart(function () {
-    $loading.show();
-  })
-  .ajaxStop(function () {
-    $loading.hide();
-  });
+})();
